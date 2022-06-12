ebe0169514e1b2d843f57ac7dfb418b81e9ba8ac
VitaliiPozniakov <vertalen89@gmail.com>
2022-06-12 17:15:08 +0300

    1

--- src/index.js
+++ src/index.js
@@ -8,53 +8,50 @@ console.log(SimpleLightbox);
 
 const imagesApiService = new ImagesApiService();
 
-const lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
-
-console.log(lightbox)
-
 
 
 const refs = {
-    form: document.querySelector(`.js-search-form`),
-    gallery: document.querySelector(`.gallery`)
-    
+  form: document.querySelector(`.js-search-form`),
+  gallery: document.querySelector(`.gallery`),
+};
+
+
+
+
+
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+
+
+  const inputSymbols = e.currentTarget.searchQuery.value;
+  //   console.log(inputSymbols)
+  imagesApiService.query = inputSymbols;
+
+  const promiseImagesArr = imagesApiService.fetchImages();
+
+  //   console.log(promiseImagesArr)
+
+  promiseImagesArr.then(makeImageMarkup).then(renderImageCard).catch(showError);
+
+  
 }
-
-refs.form.addEventListener('submit', onFormSubmit)
-
-
-function onFormSubmit (e) {
-    e.preventDefault();
-
-    const inputSymbols = e.currentTarget.searchQuery.value;
-    //   console.log(inputSymbols)
-      imagesApiService.query = inputSymbols
-
-      const promiseImagesArr = imagesApiService.fetchImages();
-
-    //   console.log(promiseImagesArr)
-
-      promiseImagesArr
-      .then(makeImageMarkup)
-      .then(renderImageCard)
-      .catch(showError);
-
-    //   const lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
-}
-
-
-
 
 function renderImageCard(imageMarkup) {
-    refs.gallery.innerHTML = imageMarkup;
-  }
+  refs.gallery.innerHTML = imageMarkup;
+}
 
-  function showError(error) {
-    //   console.log(`помилка ${error}`);
-    return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-  }
+function showError(error) {
+  //   console.log(`помилка ${error}`);
+  return Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
 
+const lightbox = new SimpleLightbox('.gallery a', { });
+lightbox.refresh()
 
-
-
-
+console.log(lightbox);
