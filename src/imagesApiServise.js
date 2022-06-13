ae43bca4208a251f3b9e504f2e823d0d10a217a4
VitaliiPozniakov <vertalen89@gmail.com>
2022-06-13 14:26:51 +0300

    add button

--- src/imagesApiServise.js
+++ src/imagesApiServise.js
@@ -1,13 +1,14 @@
 export default class ImagesApiService {
   constructor() {
     this.searchQuery = '';
+    this.page = 1
   }
 
   // https://pixabay.com/api/?key=28004990-f3c49f187ad64f64267c5955f&q=yellow+flowers&image_type=photo
 
 
   fetchImages() {
-    //   console.log(this)
+      // console.log(this)
     const BASE_URL = 'https://pixabay.com/api';
     const API_KEY = '28004990-f3c49f187ad64f64267c5955f'
 
@@ -17,6 +18,8 @@ export default class ImagesApiService {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true',
+      per_page: 40,
+      page: this.page
     });
 
     const url = `${BASE_URL}/?${searchParams}`;
@@ -28,11 +31,23 @@ export default class ImagesApiService {
         if (!r.ok) {
           throw new Error(r.status);
         }
+        this.incrementPage()
+        // console.log(this)
         return r.json();
       })
+      // .then(data => {
+      //   this.page += 1
+      // })
 
   }
 
+  resetPage () {
+    this.page = 1
+  }
+
+  incrementPage () {
+    this.page +=1
+  }
   get query() {
     return this.searchQuery;
   }
--- src/index.js
+++ src/index.js
@@ -4,54 +4,110 @@ import ImagesApiService from './ImagesApiServise';
 import { makeImageMarkup } from './makeImageMarkup';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-console.log(SimpleLightbox);
+import BtnLoadMore from './btn-load-more';
+// console.log(SimpleLightbox);
 
 const imagesApiService = new ImagesApiService();
+const btnLoadMore = new BtnLoadMore({
+  selector: `[data-action="load-more"]`,
+  hidden: true
+})
+
+console.log(btnLoadMore)
+
+
 
 
 
 const refs = {
   form: document.querySelector(`.js-search-form`),
   gallery: document.querySelector(`.gallery`),
+  btnLoadMore: document.querySelector(`[data-action="load-more"]`)
 };
 
-
-
-
-
+// console.log(refs.btnLoadMore)
 
 refs.form.addEventListener('submit', onFormSubmit);
+// refs.btnLoadMore.addEventListener(`click`, onBtnLoadMoreClick)
+btnLoadMore.refs.button.addEventListener(`click`, onBtnLoadMoreClick)
+
+
 
 function onFormSubmit(e) {
   e.preventDefault();
 
+  imagesApiService.query = e.currentTarget.elements.searchQuery.value;
+    // console.log(inputSymbols)
 
-
-  const inputSymbols = e.currentTarget.searchQuery.value;
-  //   console.log(inputSymbols)
-  imagesApiService.query = inputSymbols;
+    btnLoadMore.show()
+    btnLoadMore.disable()
+imagesApiService.resetPage()
 
   const promiseImagesArr = imagesApiService.fetchImages();
 
   //   console.log(promiseImagesArr)
-
-  promiseImagesArr.then(makeImageMarkup).then(renderImageCard).catch(showError);
-
   
+
+  promiseImagesArr
+  .then(r => {
+    // console.log(r)
+    // console.log(r.hits.length)
+    if (r.hits.length === 0) {
+      Notify.info('Sorry, there are no images matching your search query. Please try again.');
+      refs.gallery.innerHTML = '';
+      return;
+    }})
+
+    promiseImagesArr.then(makeImageMarkup).then(
+      imageMarkup => {
+        clearContainer()
+        renderImageCard(imageMarkup)
+        console.log(imageMarkup)
+      
+        btnLoadMore.enable()
+      }).catch(showError)
+
+    
+    // then(renderImageCard).catch(showError)
+    
+    // clearContainer()
+    //     btnLoadMore.enable()
+    // .then(
+    //   imageMarkup => {
+    //     renderImageCard(imageMarkup)
+    //     // console.log(imageMarkup)
+    //     clearContainer()
+    //     btnLoadMore.enable()
+    //   })
+
+ 
+ 
 }
 
+
+function onBtnLoadMoreClick (e) {
+  const promiseImagesArr = imagesApiService.fetchImages();
+  promiseImagesArr.then(makeImageMarkup).then(renderImageCard).catch(showError)
+}
+
+
 function renderImageCard(imageMarkup) {
-  refs.gallery.innerHTML = imageMarkup;
+  refs.gallery.insertAdjacentHTML('beforeend', imageMarkup)  ;
 }
 
 function showError(error) {
   //   console.log(`помилка ${error}`);
   return Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
+    'Ups'
   );
 }
 
-const lightbox = new SimpleLightbox('.gallery a', { });
-lightbox.refresh()
+function clearContainer () {
+  refs.gallery.innerHTML = ''
+}
 
-console.log(lightbox);
+const lightbox = new SimpleLightbox('.gallery a', {});
+lightbox.refresh();
+
+// console.log(lightbox);
+
